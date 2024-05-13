1682443f2a7b01f15addd8524ee3805780e4fff5
Luda68 <pril3pskay@yandex.ru>
2024-05-13 17:37:30 +0500

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,15 @@
+my_list=["kiwi","apple","bananа","peach"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:4])
+my_list[2]="orange"
+print(my_list)
+my_dict={"Bananа":"банан","Orange":"апельсин","Peach":"персик"}
+print(my_dict)
+print(my_dict["Orange"])
+my_dict["Apple"]="яблоко"
+print(my_dict)
+
+
+
